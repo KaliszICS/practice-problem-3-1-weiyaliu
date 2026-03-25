e5eca8850beb968f2d6518a66cba047a1a6d6f38
weiyaliu <343445839@tdsb.ca>
2026-03-24 22:14:50 -0400

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,30 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		System.ou.println("test1");
+	
 	}
 
+	public static String evenOrOdd (int num) {
+		if (num % 2 == 0) {
+			return "Even";
+	} else {
+			return "Odd";
+	}
+
+	public static String teacherOrStudent(String name) {
+    if (name.equals("Kalisz")) {
+        return "Teacher";
+    } else {
+        return "Student";
+    }
+
+	public static int fartherFromZero(int num) {
+    if (num > 0) {
+        return num + 5;
+    } else if (num < 0) {
+        return num - 5;
+    } else {
+        return 0;
+    }
+}
 }
